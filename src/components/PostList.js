c7a895b63945729b32038ecc9ee11c88c5c59269
sethcarrino <sethcarrino@gmail.com>
2017-09-02 19:19:21 -0400

    pulling data - working on rendering data to the page

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,15 +5,55 @@ class PostList extends Component {
     super(props);
 
     this.state = {
-      posts: []
+      author: '',
+      title: '',
+      entry: ''
     }
 
-    this.fetchData = this.fetchData.bind(this);
+    this.handleAuthor = this.handleAuthor.bind(this);
+    this.handleTitle = this.handleTitle.bind(this);
+    this.handleEntry = this.handleEntry.bind(this);
+  }
+
+  handleAuthor(e) {
+    this.setState({author: e.target.value});
+  }
+
+  handleTitle(e) {
+    this.setState({title: e.target.value});
+  }
+
+  handleEntry(e) {
+    this.setState({entry: e.target.value});
+  }
+
+  // creating a function to call that will add to the list of posts
+  addToList = (e) => {
+      e.preventDefault();
+      this.setState({author: e.target.value, title: e.target.value, entry: e.target.value});
+      let listItem = JSON.stringify(this.state);
+
+      fetch("https://tiny-lasagna-server.herokuapp.com/collections/blogger/", {
+        method: "POST",
+        body: listItem,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      }
+    }
+    ).then(response => {
+      console.log(response, "yay");
+
+    }).catch(err => {
+      console.log(err, "boo!");
+    });
+    this.setState({author: '', title: '', entry: ''});
   }
 
 
+
   componentDidMount() {
-    fetch(URL)
+    fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/')
       .then(results => {
         return results.json();
       })
@@ -23,25 +63,23 @@ class PostList extends Component {
       });
   }
 
-  fetchData = e => {
-    e.preventDefault();
-    fetch(URL)
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        this.setState({ posts: data });
-      });
-  };
-
   render() {
     const { id } = this.props.match.params;
     const URL = `https://tiny-lasagna-server.herokuapp.com/collections/blogger/${id}`;
-    return (
-      <div>
-
-      </div>
-    );
+    let blogs = this.props.posts.map( post => {
+      return (
+          <ul className="card" key={post._id}>
+            <li>
+              {post.title}
+            </li>
+          </ul>
+        );
+      });
+      return (
+        <div className="cards">
+          {blogs}
+        </div>
+      );
   }
 }
 
